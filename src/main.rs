40a7b8f3b325e8fce58d0de52595ca5ec0a3f76e
Simon Goller <neosam+github@posteo.de>
2022-11-01 13:12:01 +0100

    Add ability to drag and drop tiles

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@
 
 use bevy::{
     prelude::*,
-    render::mesh::{Indices, PrimitiveTopology},
+    render::{
+        camera::Projection,
+        mesh::{Indices, PrimitiveTopology},
+    },
 };
 
 fn main() {
@@ -14,11 +17,23 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(select_tile)
+        .add_system(drag_start_end)
+        .add_system(drag_update)
         .run();
 }
 
 #[derive(Component)]
 pub struct Tile;
+
+#[derive(Component)]
+pub struct Selected {
+    transform: Transform,
+}
+
+pub struct Drag {
+    start_pos: Vec2,
+    in_progress: bool,
+}
 
 pub struct Materials {
     default_tile: Handle<StandardMaterial>,
@@ -78,7 +93,7 @@ fn setup(
                 transform: Transform::from_xyz(
                     space * x as f32 - width as f32 / 2.0,
                     space * (height - y) as f32 - height as f32 / 2.0,
-                    0.0,
+                    (y * width + x) as f32 / (width * height) as f32 + 1.0,
                 ),
                 ..Default::default()
             };
@@ -87,6 +102,10 @@ fn setup(
     }
 
     commands.insert_resource(materials);
+    commands.insert_resource(Drag {
+        start_pos: Vec2::ZERO,
+        in_progress: false,
+    });
 }
 
 fn generate_tile_mesh(upper_left: Vec2, lower_right: Vec2) -> Mesh {
@@ -124,23 +143,36 @@ fn generate_tile_mesh(upper_left: Vec2, lower_right: Vec2) -> Mesh {
 }
 
 pub fn select_tile(
-    mut tile_query: Query<(&GlobalTransform, &mut Handle<StandardMaterial>), With<Tile>>,
+    mut tile_query: Query<
+        (
+            Entity,
+            &Transform,
+            &GlobalTransform,
+            &mut Handle<StandardMaterial>,
+        ),
+        With<Tile>,
+    >,
     camera_query: Query<(&Camera, &GlobalTransform)>,
     mut mouse_motion_events: EventReader<CursorMoved>,
     materials: Res<Materials>,
+    mut commands: Commands,
+    mut drag: ResMut<Drag>,
 ) {
+    if drag.in_progress {
+        return;
+    }
     for event in mouse_motion_events.iter() {
-        bevy::log::info!("Mouse event");
+        drag.start_pos = event.position.clone();
         let (camera, camera_transform): (&Camera, &GlobalTransform) = camera_query.single();
-        for (tile_transform, mut material) in tile_query.iter_mut() {
+        for (entity, tile_transform, tile_global_transform, mut material) in tile_query.iter_mut() {
             if let (Some(lower_left), Some(upper_right)) = (
                 camera.world_to_viewport(
                     camera_transform,
-                    tile_transform.translation() + Vec3::new(-1.0, -1.0, 0.0),
+                    tile_global_transform.translation() + Vec3::new(-1.0, -1.0, 0.0),
                 ),
                 camera.world_to_viewport(
                     camera_transform,
-                    tile_transform.translation() + Vec3::new(1.0, 1.0, 0.0),
+                    tile_global_transform.translation() + Vec3::new(1.0, 1.0, 0.0),
                 ),
             ) {
                 if event.position.x >= lower_left.x
@@ -148,12 +180,94 @@ pub fn select_tile(
                     && event.position.x <= upper_right.x
                     && event.position.y <= upper_right.y
                 {
-                    bevy::log::info!("Hover!");
                     *material = materials.highlighted_tile.clone();
+                    commands.entity(entity).insert(Selected {
+                        transform: tile_transform.clone(),
+                    });
                 } else {
                     *material = materials.default_tile.clone();
+                    commands.entity(entity).remove::<Selected>();
                 }
             }
         }
     }
+}
+
+pub fn drag_start_end(mut drag: ResMut<Drag>, mouse_button_input: Res<Input<MouseButton>>) {
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        drag.in_progress = true;
+    }
+    if mouse_button_input.just_released(MouseButton::Left) {
+        drag.in_progress = false;
+    }
+}
+
+pub fn drag_update(
+    drag: Res<Drag>,
+    mut mouse_motion_events: EventReader<CursorMoved>,
+    camera_query: Query<(&Camera, &GlobalTransform, &Projection)>,
+    mut selected_query: Query<(&mut Transform, &Selected)>,
+) {
+    if !drag.in_progress {
+        return;
+    }
+    let (camera, camera_transform, projection): (&Camera, &GlobalTransform, &Projection) =
+        camera_query.single();
+    let projection = match projection {
+        Projection::Orthographic(x) => x,
+        _ => return,
+    };
+    for event in mouse_motion_events.iter() {
+        let cursor_screen_position = event.position;
+        let cursor_world_coordinate = viewport_to_world_coordinate(
+            projection,
+            camera,
+            camera_transform,
+            cursor_screen_position.as_uvec2(),
+        );
+        let initial_world_coordinate = viewport_to_world_coordinate(
+            projection,
+            camera,
+            camera_transform,
+            drag.start_pos.as_uvec2(),
+        );
+        let world_coordinate_offset = cursor_world_coordinate - initial_world_coordinate;
+        for (mut transform, selected) in selected_query.iter_mut() {
+            let z = transform.translation.z;
+            transform.translation = add_z(
+                discard_z(selected.transform.translation) + world_coordinate_offset,
+                z,
+            );
+        }
+    }
+}
+
+pub fn discard_z(vec: Vec3) -> Vec2 {
+    Vec2::new(vec.x, vec.y)
+}
+pub fn add_z(vec: Vec2, z: f32) -> Vec3 {
+    Vec3::new(vec.x, vec.y, z)
+}
+
+pub fn viewport_to_world_coordinate(
+    projection: &OrthographicProjection,
+    camera: &Camera,
+    camera_transform: &GlobalTransform,
+    position: UVec2,
+) -> Vec2 {
+    let projection_size = Vec2::new(
+        projection.right - projection.left,
+        projection.top - projection.bottom,
+    );
+    let viewport = camera.physical_viewport_rect().unwrap();
+    let viewport_size = camera.physical_viewport_size().unwrap();
+    let relative_position = Vec2::new(
+        (position.x - viewport.0.x) as f32 / viewport_size.x as f32,
+        (position.y - viewport.0.y) as f32 / viewport_size.y as f32,
+    );
+    let origin_position = (relative_position * projection_size
+        + Vec2::new(projection.left, projection.bottom))
+        * projection.scale
+        * 2.0;
+    origin_position + discard_z(camera_transform.translation())
 }
